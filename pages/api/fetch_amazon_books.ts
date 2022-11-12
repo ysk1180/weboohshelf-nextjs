85e2410bc9d77e4f8f254292fc77862f8fcd0c24
Yusuke Miyazaki <ysk1180@gmail.com>
2022-11-12 10:32:51 +0900

    発売日が入らないバグがあったので修正

--- pages/api/fetch_amazon_books.ts
+++ pages/api/fetch_amazon_books.ts
@@ -32,7 +32,7 @@ const fetchAmazonBooks = async (
     let formattedD = ''
     if (item.ItemInfo.ContentInfo.PublicationDate?.DisplayValue) {
       const d = new Date(item.ItemInfo.ContentInfo.PublicationDate?.DisplayValue)
-      const formattedD = `${d.getFullYear()}/${(d.getMonth() + 1).toString().padStart(2, '0')}/${d.getDate().toString().padStart(2, '0')}`
+      formattedD = `${d.getFullYear()}/${(d.getMonth() + 1).toString().padStart(2, '0')}/${d.getDate().toString().padStart(2, '0')}`
     }
     returnData.push({
       asin: item.ASIN,
